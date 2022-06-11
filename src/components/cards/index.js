eebf07defe1d492acce27819d19ca1ea0515a94d
zehrabarann <brnzehraa@gmail.com>
2022-06-11 16:11:50 +0300

    Create card and slider component

---
+++ src/components/cards/index.js
@@ -0,0 +1,21 @@
+import { Card } from 'antd';
+
+const { Meta } = Card;
+
+const Cards = () => {
+
+    
+    return (
+        <>
+            <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            >
+                <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card>
+        </>
+    )
+}
+
+export default Cards
--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,11 +1,15 @@
 import { Carousel } from 'antd';
-
+import image1 from "../../asset/image.jpeg"
+import image2 from "../../asset/image2.jpeg"
 const Slider = () => {
     return (
         <>
             <Carousel autoplay>
                 <div>
-                    
+                    <img src={image1} alt="wallpaper"/>
+                </div>
+                <div>
+                    <img src={image2} alt="wallpaper"/>
                 </div>
              
             </Carousel>
